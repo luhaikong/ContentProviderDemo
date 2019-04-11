420453169996e7f9896dfac21678a56228148f89
luhaikong <1031350293@qq.com>
2019-04-11 18:13:37 +0800

    1、运行main进程 2、运行zonghe进程

---
+++ app/src/main/java/com/lhk/cpdemo/DatabaseProvider.java
@@ -0,0 +1,168 @@
+package com.lhk.cpdemo;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Administrator on 2019/4/10.
+ */
+
+public class DatabaseProvider extends ContentProvider {
+
+    private Context mContext;
+    DBHelper mDbHelper = null;
+    SQLiteDatabase db = null;
+
+    public static final String AUTOHORITY = "cn.scu.myprovider";
+    // 设置ContentProvider的唯一标识
+
+    public static final int User_Code = 1;
+    public static final int Job_Code = 2;
+
+    // UriMatcher类使用:在ContentProvider 中注册URI
+    private static final UriMatcher mMatcher;
+    static{
+        mMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        // 初始化
+        mMatcher.addURI(AUTOHORITY,"user", User_Code);
+        mMatcher.addURI(AUTOHORITY, "job", Job_Code);
+        // 若URI资源路径 = content://cn.scu.myprovider/user ，则返回注册码User_Code
+        // 若URI资源路径 = content://cn.scu.myprovider/job ，则返回注册码Job_Code
+    }
+
+
+
+    /**
+     * ContentProvider创建后 或 打开系统后其它进程第一次访问该ContentProvider时 由系统进行调用
+     * 注：运行在ContentProvider进程的主线程，故不能做耗时操作
+     * @return
+     */
+    @Override
+    public boolean onCreate() {
+        mContext = getContext();
+        // 在ContentProvider创建时对数据库进行初始化
+        // 运行在主线程，故不能做耗时操作,此处仅作展示
+        mDbHelper = new DBHelper(getContext());
+        db = mDbHelper.getWritableDatabase();
+
+        // 初始化两个表的数据(先清空两个表,再各加入一个记录)
+        db.execSQL("delete from user");
+        db.execSQL("insert into user values(1,'Carson');");
+        db.execSQL("insert into user values(2,'Kobe');");
+
+        db.execSQL("delete from job");
+        db.execSQL("insert into job values(1,'Android');");
+        db.execSQL("insert into job values(2,'iOS');");
+
+        return true;
+    }
+
+    /**
+     * 外部应用 获取 ContentProvider 中的数据
+     * @param uri
+     * @param projection
+     * @param selection
+     * @param selectionArgs
+     * @param sortOrder
+     * @return
+     */
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        // 根据URI匹配 URI_CODE，从而匹配ContentProvider中相应的表名
+        // 该方法在最下面
+        String table = getTableName(uri);
+
+//        // 通过ContentUris类从URL中获取ID
+//        long personid = ContentUris.parseId(uri);
+//        System.out.println(personid);
+
+        // 查询数据
+        return db.query(table,projection,selection,selectionArgs,null,null,sortOrder,null);
+    }
+
+    /**
+     * 得到数据类型，即返回当前 Url 所代表数据的MIME类型
+     * @param uri
+     * @return
+     */
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    /**
+     * 外部进程向 ContentProvider 中添加数据
+     * @param uri
+     * @param values
+     * @return
+     */
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        // 根据URI匹配 URI_CODE，从而匹配ContentProvider中相应的表名
+        // 该方法在最下面
+        String table = getTableName(uri);
+
+        // 向该表添加数据
+        db.insert(table, null, values);
+
+        // 当该URI的ContentProvider数据发生变化时，通知外界（即访问该ContentProvider数据的访问者）
+        mContext.getContentResolver().notifyChange(uri, null);
+
+//        // 通过ContentUris类从URL中获取ID
+//        long personid = ContentUris.parseId(uri);
+//        System.out.println(personid);
+
+        return uri;
+    }
+
+    /**
+     * 外部进程 删除 ContentProvider 中的数据
+     * @param uri
+     * @param selection
+     * @param selectionArgs
+     * @return
+     */
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    /**
+     * 外部进程更新 ContentProvider 中的数据
+     * @param uri
+     * @param values
+     * @param selection
+     * @param selectionArgs
+     * @return
+     */
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    /**
+     * 根据URI匹配 URI_CODE，从而匹配ContentProvider中相应的表名
+     */
+    private String getTableName(Uri uri){
+        String tableName = null;
+        switch (mMatcher.match(uri)) {
+            case User_Code:
+                tableName = DBHelper.USER_TABLE_NAME;
+                break;
+            case Job_Code:
+                tableName = DBHelper.JOB_TABLE_NAME;
+                break;
+        }
+        return tableName;
+    }
+}
